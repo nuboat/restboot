e824490cfbb5c998b839fa68349ef087d2efe983
Peerapat A <peerapat.asoktummarungsri@agoda.com>
2016-07-01 13:57:10 +0700

    Create Project

---
+++ src/main/java/oathailand/restboot/controllers/HelloController.java
@@ -0,0 +1,22 @@
+package oathailand.restboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * If you annotate a method with @ResponseBody,
+ * spring will try to convert its return value and write it to the http response automatically.
+ *
+ * @author peerapat
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello World !";
+    }
+
+}
